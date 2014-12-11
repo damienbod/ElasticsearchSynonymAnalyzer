e4f6e7263900355878dba507ae8f1695c948074c
damienbod <damien_bod@hotmail.com>
2014-12-11 08:36:32 +0100

    init

---
+++ ElasticsearchSynonymAnalyzer/ElasticsearchProvider.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using ElasticsearchCRUD;
+using ElasticsearchCRUD.ContextAddDeleteUpdate.IndexModel.SettingsModel;
+using ElasticsearchCRUD.ContextAddDeleteUpdate.IndexModel.SettingsModel.Analyzers;
+using ElasticsearchCRUD.ContextAddDeleteUpdate.IndexModel.SettingsModel.Filters;
+using ElasticsearchCRUD.Model;
+using ElasticsearchCRUD.Tracing;
+
+namespace ElasticsearchSynonymAnalyzer
+{
+	public class ElasticsearchMemberProvider
+	{
+		public ElasticsearchMemberProvider()
+		{
+			_context = new ElasticsearchContext(ConnectionString, new ElasticsearchSerializerConfiguration(_elasticsearchMappingResolver))
+			{
+				TraceProvider = new ConsoleTraceProvider()
+			};
+		}
+		private readonly IElasticsearchMappingResolver _elasticsearchMappingResolver = new ElasticsearchMappingResolver();
+		private const string ConnectionString = "http://localhost:9200";
+		private readonly ElasticsearchContext _context;
+
+		public IndexDefinition CreateNewIndexDefinitionForSynonymFilter()
+		{
+			return new IndexDefinition
+			{
+				IndexSettings =
+				{
+					Analysis = new Analysis
+					{
+						Analyzer =
+						{
+							Analyzers = new List<AnalyzerBase>
+							{
+								new CustomAnalyzer("my_analyzer")
+								{
+									Tokenizer = DefaultTokenizers.Standard,
+									Filter = new List<string> {DefaultTokenFilters.Standard, DefaultTokenFilters.Lowercase, "john_synonym"}
+								}
+							}
+						},
+						Filters =
+						{
+							CustomFilters = new List<AnalysisFilterBase>
+							{
+								new SynonymTokenFilter("john_synonym")
+								{
+									Synonyms = new List<string>
+									{
+										"sean  => john",
+										"séan => john",
+										"johny => john",
+									}
+								}
+							}
+						}
+					},
+					NumberOfShards = 3,
+					NumberOfReplicas = 1
+				},
+			};
+
+		}
+
+		public void CreateIndex(IndexDefinition indexDefinition)
+		{
+			_context.IndexCreate<Member>(indexDefinition);
+		}
+
+		public void CreateSomeMembers()
+		{
+			var jm = new Member {Id = 1, FamilyName = "Moore", Info = "In the club since 1976", Name = "John"};
+			_context.AddUpdateDocument(jm, jm.Id);
+
+			_context.SaveChanges();
+		}
+
+	}
+}
---
+++ ElasticsearchSynonymAnalyzer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ElasticsearchSynonymAnalyzer
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			var elasticsearchMemberProvider = new ElasticsearchMemberProvider();
+
+			// Create the index defintion
+			var indexDefinition = elasticsearchMemberProvider.CreateNewIndexDefinitionForSynonymFilter();
+
+			// create a new index and type mapping in elasticseach
+			elasticsearchMemberProvider.CreateIndex(indexDefinition);
+
+			elasticsearchMemberProvider.CreateSomeMembers();
+
+			// TODO create search with expected results
+		}
+	}
+
+	public class Member
+	{
+		public long Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string FamilyName { get; set; }
+
+		public string Info { get; set; }
+	}
+}
